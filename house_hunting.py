05f743d93c1f4baf461b00e7563e8dee797b6921
Calixd7 <calixd7@gmail.com>
2020-12-07 20:06:28 -0500

    Finished working code

--- house_hunting.py
+++ house_hunting.py
@@ -1,1 +1,20 @@
 # Write your code here
+annual_salary = int(input('Enter your annual salary: '))
+portion_saved = float(input('Enter the percent of your salary to save, as decimal: '))
+total_cost = int(input('Enter the cost of your dream home: '))
+
+
+current_savings = 0
+r = .04
+portion_down_payment= .25
+number_of_months = 0
+down_payment = total_cost * portion_down_payment
+
+while current_savings < down_payment:
+   number_of_months += 1
+   current_savings += current_savings * r/12
+   current_savings += annual_salary * portion_saved /12
+  
+
+ 
+print("Number of months: ", number_of_months)    
